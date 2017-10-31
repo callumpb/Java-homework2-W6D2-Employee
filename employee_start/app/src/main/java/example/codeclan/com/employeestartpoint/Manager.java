a761df974226fa5e327272311efdf447178047ab
Callum <callumpb@hotmail.com>
2017-10-31 22:59:16 +0000

    homework + errors to be changed

---
+++ employee_start/app/src/main/java/example/codeclan/com/employeestartpoint/Manager.java
@@ -0,0 +1,13 @@
+package example.codeclan.com.employeestartpoint;
+
+/**
+ * Created by user on 31/10/2017.
+ */
+
+public class Manager extends Employee {
+
+    //might need to include deptName things
+    public Manager(int id, String name, String socialSecurityNumber, double salary, String deptName) {
+        super(id, name, socialSecurityNumber, salary, deptName);
+    }
+}
